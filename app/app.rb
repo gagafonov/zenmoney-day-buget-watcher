71a8f0e2fba8c99f5694f76214431241e15be50d
Georgiy Agafonov <g.agafonov@corp.webregul.ru>
2025-10-31 16:30:53 +0000

    app, removed panned outcomes, fixed net errors processing

--- app/app.rb
+++ app/app.rb
@@ -50,136 +50,126 @@ while true
   todayOutcomeCalc = (appOutcomeTotal / daysInMonth).to_i
   totalOutcomeCalc = todayOutcomeCalc * todayDay
 
-  zenMoneyApiJson = Net::HTTP.start(zenMoneyApiUri.host, zenMoneyApiUri.port, :use_ssl => true) { |http|
-    apiRequest = Net::HTTP::Post.new(zenMoneyApiUri)
-    apiRequest['Authorization'] = "Bearer #{zenMoneyApiToken}"
-    apiRequest['Content-Type'] = 'application/json'
-    apiRequest.body = {
-      lastServerTimestamp: startTimestamp,
-      currentClientTimestamp: endTimestamp,
-    }.to_json
+  begin
+    zenMoneyApiJson = Net::HTTP.start(zenMoneyApiUri.host, zenMoneyApiUri.port, :use_ssl => true) { |http|
+      apiRequest = Net::HTTP::Post.new(zenMoneyApiUri)
+      apiRequest['Authorization'] = "Bearer #{zenMoneyApiToken}"
+      apiRequest['Content-Type'] = 'application/json'
+      apiRequest.body = {
+        lastServerTimestamp: startTimestamp,
+        currentClientTimestamp: endTimestamp,
+      }.to_json
 
-    apiResponse = http.request(apiRequest)
-    if apiResponse.code.to_i == 200
-      JSON.parse(apiResponse.body)
-    else
-      raise "Api response returns no 200 code = #{apiResponse.code}"
-    end
-  }
-
-  plannedOutcome = 0
-  if zenMoneyApiJson.key?('reminderMarker')
-    puts 'Planned - process'
-
-    plannedOutcome = zenMoneyApiJson['reminderMarker']
-      .select{|t| t['date'] == todayDateStr}
-      .select{|t| zenMoneyCheckAccounts.include?(t['outcomeAccount'])}
-      .select{|t| t['state'] == 'planned'}
-      .sum{|t| t['outcome']}
+      apiResponse = http.request(apiRequest)
+      if apiResponse.code.to_i == 200
+        JSON.parse(apiResponse.body)
+      else
+        puts "Api response returns no 200 code: #{apiResponse.code}"
+      end
+    }
+  rescue => e
+    puts "Cannot connect to #{zenMoneyApiUri.host}: #{e.message}"
   end
 
-  if zenMoneyApiJson.key?('transaction')
-    puts 'Transactions - process'
+  unless zenMoneyApiJson.nil?
+    if zenMoneyApiJson.key?('transaction')
+      puts 'Transactions - process'
 
-    outcomeFact = zenMoneyApiJson['transaction']
-      .select {|t| ! t['hold'].nil?}
-      .select {|t| ! t['deleted']}
-      .select {|t| zenMoneyCheckAccounts.include?(t['outcomeAccount'])}
-      .select {|t| ! zenMoneyCheckExcludeIncomeAccounts.include?(t['incomeAccount'])}
+      outcomeFact = zenMoneyApiJson['transaction']
+        .select {|t| ! t['hold'].nil?}
+        .select {|t| ! t['deleted']}
+        .select {|t| zenMoneyCheckAccounts.include?(t['outcomeAccount'])}
+        .select {|t| ! zenMoneyCheckExcludeIncomeAccounts.include?(t['incomeAccount'])}
 
-    totalOutcomeFact = outcomeFact
-      .select {|t| t['date'].match?(/^#{todayYearMonthStr}\-.*$/)}
-      .sum{|t| t['outcome']}
-      .to_i
+      totalOutcomeFact = outcomeFact
+        .select {|t| t['date'].match?(/^#{todayYearMonthStr}\-.*$/)}
+        .sum{|t| t['outcome']}
+        .to_i
 
-    todayOutcomeFact = outcomeFact
-      .select {|t| t['date'] == todayDateStr}
-      .sum{|t| t['outcome']}
-      .to_i
+      todayOutcomeFact = outcomeFact
+        .select {|t| t['date'] == todayDateStr}
+        .sum{|t| t['outcome']}
+        .to_i
 
-    if totalOutcomeFact > totalOutcomeCalc
-      if telegramOutcomeNotMathMessageSended
-        puts "Problem - calculated (#{numberFormat(totalOutcomeCalc)} rur) and actual (#{numberFormat(totalOutcomeFact)} rur) total outcomes do not match!"
-      else
-        telegramMessage = []
-        telegramMessage.push('Внимание: фактический расход больше расчетного!')
-        telegramMessage.push('')
-        telegramMessage.push("→ Перерасход: #{numberFormat(totalOutcomeFact - totalOutcomeCalc)}р")
-        telegramMessage.push("→ Фактический расход: #{numberFormat(todayOutcomeFact)}р")
-        telegramMessage.push("→ Расчетный расход: #{numberFormat(todayOutcomeCalc)}р")
-
-        telegramResponse = telegramMessageSend(telegramBotId, telegramBotToken, telegramWarningsChatId, telegramMessage.join("\n"))
-        if telegramResponse.code.to_i == 200
-          telegramOutcomeNotMathMessageSended = true
-          puts 'Outcomes not match message succesfully sended'
+      if totalOutcomeFact > totalOutcomeCalc
+        if telegramOutcomeNotMathMessageSended
+          puts "Problem - calculated (#{numberFormat(totalOutcomeCalc)} rur) and actual (#{numberFormat(totalOutcomeFact)} rur) total outcomes do not match!"
         else
-          raise "Telegram response returns no 200 code = #{telegramResponse.code}"
+          telegramMessage = []
+          telegramMessage.push('Внимание: фактический расход больше расчетного!')
+          telegramMessage.push('')
+          telegramMessage.push("→ Перерасход: #{numberFormat(totalOutcomeFact - totalOutcomeCalc)}р")
+          telegramMessage.push("→ Фактический расход: #{numberFormat(todayOutcomeFact)}р")
+          telegramMessage.push("→ Расчетный расход: #{numberFormat(todayOutcomeCalc)}р")
+
+          telegramResponse = telegramMessageSend(telegramBotId, telegramBotToken, telegramWarningsChatId, telegramMessage.join("\n"))
+          if telegramResponse.code.to_i == 200
+            telegramOutcomeNotMathMessageSended = true
+            puts 'Outcomes not match message succesfully sended'
+          else
+            puts "Telegram response returns no 200 code: #{telegramResponse.code}"
+          end
         end
+      else
+        puts "Everything is fine - calculated (#{numberFormat(totalOutcomeCalc)} rur) and actual (#{numberFormat(totalOutcomeFact)} rur) total outcomes match"
       end
-    else
-      puts "Everything is fine - calculated (#{numberFormat(totalOutcomeCalc)} rur) and actual (#{numberFormat(totalOutcomeFact)} rur) total outcomes match"
+
+      if totalOutcomeFact > lastTotalOutcomeFact && lastTotalOutcomeFact > 0
+        puts 'Actual outcome changed - process'
+
+        telegramMessage = []
+        telegramMessage.push("Изменение суммы расхода: #{numberFormat(lastTotalOutcomeFact)}р → #{numberFormat(totalOutcomeFact)}р")
+        telegramMessage.push("Сумма, реально доступная для расхода: #{numberFormat(totalOutcomeCalc - totalOutcomeFact)}р")
+
+        telegramResponse = telegramMessageSend(telegramBotId, telegramBotToken, telegramNoticesChatId, telegramMessage.join("\n"))
+        if telegramResponse.code.to_i == 200
+          puts 'Actual outcome changed message succesfully sended'
+        else
+          puts "Telegram response returns no 200 code: #{telegramResponse.code}"
+        end
+      elsif lastTotalOutcomeFact == totalOutcomeFact
+        puts 'Actual outcome is not changed - skip'
+      end
+
+      lastTotalOutcomeFact = totalOutcomeFact
     end
 
-    if totalOutcomeFact > lastTotalOutcomeFact && lastTotalOutcomeFact > 0
-      puts 'Actual outcome changed - process'
+    if nowTime.hour.to_i == appSummaryMessageSendHour && ! telegramDaySummaryMessageSended
+      puts 'Day summary - process'
 
       telegramMessage = []
-      telegramMessage.push("Изменение суммы расхода: #{numberFormat(lastTotalOutcomeFact)}р → #{numberFormat(totalOutcomeFact)}р")
-      telegramMessage.push("Сумма, реально доступная для расхода: #{numberFormat(totalOutcomeCalc - totalOutcomeFact - plannedOutcome)}р")
+      telegramMessage.push('Бюджет на день')
+      telegramMessage.push('')
+      telegramMessage.push("→ Расчетная дневная сумма расхода: #{numberFormat(todayOutcomeCalc)}р")
+      telegramMessage.push("→ Сумма, реально доступная для расхода: #{numberFormat(totalOutcomeCalc - totalOutcomeFact)}р")
 
-      if plannedOutcome > 0
-        telegramMessage.push("Сумма запланированных расходов: #{numberFormat(plannedOutcome)}р")
-      end
-
-      telegramResponse = telegramMessageSend(telegramBotId, telegramBotToken, telegramNoticesChatId, telegramMessage.join("\n"))
+      telegramResponse = telegramMessageSend(telegramBotId, telegramBotToken, telegramSummaryChatId, telegramMessage.join("\n"))
       if telegramResponse.code.to_i == 200
-        puts 'Actual outcome changed message succesfully sended'
+        telegramDaySummaryMessageSended = true
+        puts 'Day summary message succesfully sended'
       else
-        raise "Telegram response returns no 200 code = #{telegramResponse.code}"
+        puts "Telegram response returns no 200 code: #{telegramResponse.code}"
       end
-    elsif lastTotalOutcomeFact == totalOutcomeFact
-      puts 'Actual outcome is not changed - skip'
     end
 
-    lastTotalOutcomeFact = totalOutcomeFact
-  end
-
-  if nowTime.hour.to_i == appSummaryMessageSendHour && ! telegramDaySummaryMessageSended
-    puts 'Day summary - process'
-
-    telegramMessage = []
-    telegramMessage.push('Бюджет на день')
-    telegramMessage.push('')
-    telegramMessage.push("→ Расчетная дневная сумма расхода: #{numberFormat(todayOutcomeCalc)}р")
-    telegramMessage.push("→ Сумма запланированных расходов: #{numberFormat(plannedOutcome)}р")
-    telegramMessage.push("→ Сумма, реально доступная для расхода: #{numberFormat(totalOutcomeCalc - totalOutcomeFact - plannedOutcome)}р")
-
-    telegramResponse = telegramMessageSend(telegramBotId, telegramBotToken, telegramSummaryChatId, telegramMessage.join("\n"))
-    if telegramResponse.code.to_i == 200
-      telegramDaySummaryMessageSended = true
-      puts 'Day summary message succesfully sended'
-    else
-      raise "Telegram response returns no 200 code = #{telegramResponse.code}"
+    if nowTime.hour.to_i == 0 && nowTime.min.to_i == 0
+      telegramOutcomeNotMathMessageSended = false
+      telegramDaySummaryMessageSended = false
+      telegramReminderMarkerBackupMessageSended = false
     end
-  end
 
-  if nowTime.hour.to_i == 0 && nowTime.min.to_i == 0
-    telegramOutcomeNotMathMessageSended = false
-    telegramDaySummaryMessageSended = false
-    telegramReminderMarkerBackupMessageSended = false
-  end
+    if appBackupSpendingPlansEnabled && zenMoneyApiJson.key?('reminder')
+      if nowTime.hour.to_i == appBackupSpendingPlansMessageSendHour && ! telegramReminderMarkerBackupMessageSended
+        puts 'reminder - process'
 
-  if appBackupSpendingPlansEnabled && zenMoneyApiJson.key?('reminderMarker')
-    if nowTime.hour.to_i == appBackupSpendingPlansMessageSendHour && ! telegramReminderMarkerBackupMessageSended
-      puts 'reminderMarker - process'
-
-      telegramMessage = 'Бекап ключа reminderMarker – планов расходов'
-      telegramResponse = telegramJsonSend(telegramBotId, telegramBotToken, telegramNoticesChatId, telegramMessage, JSON.pretty_generate(zenMoneyApiJson['reminderMarker']), 'reminderMarkerBackup.json')
-      if telegramResponse.code.to_i == 200
-        telegramReminderMarkerBackupMessageSended = true
-        puts 'reminderMarker backup message succesfully sended'
-      else
-        raise "Telegram response returns no 200 code = #{telegramResponse.code}"
+        telegramMessage = 'Бекап ключа reminder – планов расходов'
+        telegramResponse = telegramJsonSend(telegramBotId, telegramBotToken, telegramNoticesChatId, telegramMessage, JSON.pretty_generate(zenMoneyApiJson['reminder']), 'reminderBackup.json')
+        if telegramResponse.code.to_i == 200
+          telegramReminderMarkerBackupMessageSended = true
+          puts 'Reminder backup message succesfully sended'
+        else
+          puts "Telegram response returns no 200 code: #{telegramResponse.code}"
+        end
       end
     end
   end
